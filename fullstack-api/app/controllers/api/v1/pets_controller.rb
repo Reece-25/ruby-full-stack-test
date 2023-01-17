4c90c9158135f107d971b389b7d9f841be46e3f8
Reece Cope <reececope25@gmail.com>
2023-01-17 18:29:23 +0000

    Add scope to show pets that are featured first then alphabetically

--- fullstack-api/app/controllers/api/v1/pets_controller.rb
+++ fullstack-api/app/controllers/api/v1/pets_controller.rb
@@ -5,7 +5,7 @@ module Api
     class PetsController < ApplicationController
 
       def index
-        pets = Pet.all
+        pets = Pet.all.featured_alphabetical
         render json: pets, status: :ok
       end
 
--- fullstack-api/app/models/pet.rb
+++ fullstack-api/app/models/pet.rb
@@ -1,2 +1,5 @@
 class Pet < ApplicationRecord
+
+  scope :featured_alphabetical, -> { order(featured: :asc, name: :asc)}
+
 end
